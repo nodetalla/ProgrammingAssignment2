2ccfbd3119a51fd4bb2121fa42d547c27c41d0bf
nodetalla <noor_od@hotmail.com>
2016-02-21 01:56:00 -0500

    Caching the inverse of a Matrix
    For a given Matrix, check if the inverse matrix is available in memory
    and cache it. If the inverse is not available then calculate it and save
    it in the memory.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix function
+## two functions that are used to create a matrix, calculate it's inverse
+## before calculating the inverse, the functions check if the inverse is already calculated
+## if the inverse is calculated and the matrix did not change the inverse is cached 
+## from the memory instead of calculating it again
 
-## Write a short comment describing this function
+## function-1 makecacheMatrix
+##uses an input matrix to create a list that contains the matrix and it's inverse matrix 
+##the function can be used to create the matrix and calculate it's inverse matrix 
+##or cache its previously calculated values from memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## function-2 cacheSolve
+##checks if the inverse matrix was calculated before and gets it from the cache
+## When loaded from memory, the function displays a message to inform the user
+## if the inverse matrix is not available it calculates and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #check if the inverse matrix exists in memory       
+        inv <- x$getinverse()
+        
+        if(!is.null(inv)) {
+                # if the inverse is available cache it and exit without calculations
+                message("Invers matrix available in cache memory, loading cached data...")
+                return(inv)
+        }
+        
+        #if the inverse matrix is not available solve for it and return it
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+
 }
